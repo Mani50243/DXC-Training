55dac566786cc1494c24dfd3412524d907e08614
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2020-09-30 13:17:23 +0530

    Add files via upload

---
+++ ESubscriber/src/main/java/com/eureka/SubScriber/ESubscriber/Model/Employee.java
@@ -0,0 +1,37 @@
+package com.eureka.SubScriber.ESubscriber.Model;
+
+public class Employee {
+	
+	String empName;
+	public int getEmpSalary() {
+	return empSalary;
+	}
+
+	public void setEmpSalary(int empSalary) {
+	this.empSalary = empSalary;
+	}
+	int empSalary;
+	public Employee() {
+	// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+	return "Employee [empName=" + empName + ", empSalary=" + empSalary + "]";
+	}
+
+
+
+	public Employee(String empName, int empSalary) {
+	super();
+	this.empName = empName;
+	this.empSalary = empSalary;
+	}
+	public String getEmpName() {
+	return empName;
+	}
+	public void setEmpName(String empName) {
+	this.empName = empName;
+	}
+
+}
