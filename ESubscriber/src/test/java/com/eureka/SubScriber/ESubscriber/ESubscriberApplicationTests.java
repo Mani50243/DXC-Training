6ba6fbb369d1171ad7a70e3aea3de2a1479a10ed
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2020-09-30 13:15:26 +0530

    Add files via upload

---
+++ ESubscriber/src/test/java/com/eureka/SubScriber/ESubscriber/ESubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.SubScriber.ESubscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESubscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
