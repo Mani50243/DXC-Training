f3113ca30cf2b1d1cd94ced529b87ac678e44d10
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2020-09-30 13:14:45 +0530

    Add files via upload

---
+++ EServer/src/test/java/com/Server/eureka/EServer/EServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Server.eureka.EServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
